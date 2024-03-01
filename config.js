c038480bbc7e45466fe4e98ead1f3f8dcdd672cd
Sugarbobo <21107283+sugarbobo-ch@users.noreply.github.com>
2024-03-01 18:48:08 +0800

    Fix config.js typos

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ var config = {
   prefix: '!', // Read prefix of commnad
   username: 'YOUR_USER_NAME', // Your username
   // Your access token here, do not let third person know this, get from: https://twitchtokengenerator.com/
-  token: 'YOUR_ASSET_TOKEN',
+  token: 'YOUR_ACCESS_TOKEN',
   channel: 'YOUR_CHANNEL_ID'
 }
